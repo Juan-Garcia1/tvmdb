18708f81104782c6d7a33af2c50f356978a0ca05
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-01-02 14:52:54 -0600

    added process.env

--- src/functions/getTvShowGenres.js
+++ src/functions/getTvShowGenres.js
@@ -1,9 +1,7 @@
 import axios from "axios"
 
 exports.handler = function (event, context, callback) {
-  // const { API_URL, API_CLIENT_ID } = process.env;
-  const API_URL = "https://api.themoviedb.org/3"
-  const API_CLIENT_ID = "12a5356516535d4d67654a936a088c1b"
+  const { API_URL, API_CLIENT_ID } = process.env;
 
   const URL = `${API_URL}/genre/tv/list?api_key=${API_CLIENT_ID}`;
 
